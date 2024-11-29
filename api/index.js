835e640f4c171fd86373f520931d80996fbe2cf8
Memona Amir <157165172+MemonaAmirAbdulHaq@users.noreply.github.com>
2024-11-29 17:20:54 +0500

    add .

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user.route';
+
 dotenv.config();
 mongoose.connect(process.env.MONGO)   //add .env to gitignore 
 .then(()=>{
@@ -14,4 +14,3 @@ const app =express();
 app.listen(3000, () =>{
     console.log('server is running port 3000');
 });
-app.use('/api/user',userRouter)
